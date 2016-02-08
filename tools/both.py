06fd3c72ae475e517cf70a3fa0ca8c5c571feea8
Steven Kitzes <steven.kitzes@gmail.com>
2016-02-08 14:11:09 -0800

    updated both.py to accept an output filename modifier so that you can run multiple tests with the same request count and concurrency level

--- tools/both.py
+++ tools/both.py
@@ -5,8 +5,13 @@ num = sys.argv[1]
 con = sys.argv[2]
 name = sys.argv[3]
 
-phpcommand = "c:\\Apache24\\bin\\ab.exe -n " + num + " -c " + con + " -r http://myapp.com/" + name + ".php > c:\\abres\\php_" + num + "-" + con + "-" + name + ".txt"
-nodecommand = "c:\\Apache24\\bin\\ab.exe -n " + num + " -c " + con + " -r http://myapp.com:3000/" + name + "/ > c:\\abres\\node_" + num + "-" + con + "-" + name + ".txt"
+args = len(sys.argv)
+mod = ""
+if(args > 4):
+	mod = "_" + sys.argv[4]
+
+phpcommand = "c:\\Apache24\\bin\\ab.exe -n " + num + " -c " + con + " -r http://myapp.com/" + name + ".php > c:\\abres\\php_" + num + "-" + con + "-" + name + mod + ".txt"
+nodecommand = "c:\\Apache24\\bin\\ab.exe -n " + num + " -c " + con + " -r http://myapp.com:3000/" + name + "/ > c:\\abres\\node_" + num + "-" + con + "-" + name + mod + ".txt"
 
 os.system(phpcommand)
 os.system(nodecommand)
