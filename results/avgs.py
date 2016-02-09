717611b442687bf9076296bbea38b5be0e891eb3
Steven Kitzes <steven.kitzes@gmail.com>
2016-02-08 17:52:08 -0800

    updated some scripts to add clarity in output and documentation

--- results/avgs.py
+++ results/avgs.py
@@ -1,6 +1,22 @@
 import re
 import sys
 
+if sys.argv[1]:
+	if sys.argv[1] == "-h":
+		print("Expected arguments are all mandatory and must appear in order.")
+		print("")
+		print("    python avgs.py [num] [con] [name] [which] [howmany]")
+		print("")
+		print("[num]                    - how many requests were made in the series")
+		print("[con]                    - concurrency level evaluated in the series")
+		print("[name]                   - name of the test that was run in the series")
+		print("[which]                  - 'node' or 'php' (can't average both together)")
+		print("[howmany]                - how many test result files to include")
+		print("")
+		print("This script ASSUMES that file names are well formed, and that all")
+		print("files are present, from 0 through the number (howmany) given, exclusive.")
+		sys.exit()
+
 print("Calculating some averages from given input files . . .")
 
 bandwidths = []		# in req per sec
@@ -11,16 +27,17 @@ longests = []		# in ms
 num = sys.argv[1]
 con = sys.argv[2]
 name = sys.argv[3]
-which = sys.argv[4]
+which = sys.argv[4]			# 'node' or 'php'
+howmany = int(sys.argv[5])
 
-for i in range(0, 5):
+for i in range(0, howmany):
 	incomingFile = which + "_" + num + "-" + con + "-" + name + "_" + str(i) + ".txt"
-	print("Opening file: " + incomingFile)
+	# print("Opening file: " + incomingFile)
 	
 	f = open(incomingFile)
 	fContent = f.read()
 	
-	print("File content loaded.")
+	# print("File content loaded.")
 	
 	bandwidthRE = "[0-9][\.\d]*(?=(\s\[#))"
 	bandwidthMatchGroup = re.search(bandwidthRE, fContent)
@@ -38,11 +55,11 @@ for i in range(0, 5):
 	longestMatchGroup = re.search(longestRE, fContent)
 	longests.append(float(longestMatchGroup.group(0)))
 
-print("")
-print("bandwidths: " + str(bandwidths))
-print("concurrentMeans: " + str(concurrentMeans))
-print("means: " + str(means))
-print("longests: " + str(longests))
+#print("")
+#print("bandwidths: " + str(bandwidths))
+#print("concurrentMeans: " + str(concurrentMeans))
+#print("means: " + str(means))
+#print("longests: " + str(longests))
 
 bwTot = 0
 cmTot = 0
--- tools/series-it.py
+++ tools/series-it.py
@@ -1,13 +1,29 @@
 import os
 import sys
 
+if sys.argv[1]:
+	if sys.argv[1] == "-h":
+		print("Expected arguments are all mandatory and must appear in order.")
+		print("")
+		print("    python series-it.py [num] [name] [maxCon] [desiredIterations]")
+		print("")
+		print("[num]                    - how many requests to make per series")
+		print("[name]                   - name of the test that will be run")
+		print("[maxCon]                 - concurrency level starts at two and")
+		print("                           increases by powers of two until this")
+		print("                           value is reached")
+		print("[desiredIterations       - how many times to run the same test in")
+		print("                           the series (to investigate consistency)")
+		sys.exit()
+
 num = sys.argv[1]
 name = sys.argv[2]
 maxCon = int(sys.argv[3])
+desiredIterations = sys.argv[4]
 
 currentCon = 2
 
 while currentCon < maxCon:
-	command = "python both.py " + num + " " + currentCon + " " + name + " " + str(5)
+	command = "python series.py " + num + " " + str(currentCon) + " " + name + " " + desiredIterations
 	os.system(command)
 	currentCon = 2 * currentCon
