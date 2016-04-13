99513ad954e3a3437d9be66b96b6c63558a0cffe
Steven Kitzes <winds23@gmail.com>
2016-04-13 16:17:34 -0700

    corrected test name from simpleString to db in multiple simultaneous test script multi.py

--- tools/multi.py
+++ tools/multi.py
@@ -1,7 +1,7 @@
 import subprocess
 
 def n_fireSimple():
-	subprocess.Popen(["c:/apache24/bin/ab.exe", "-n","10000","-c","64","http://192.168.0.10:3000/simpleString/",">","n_simple.txt"], shell=True, stdin=None, stdout=None, stderr=None, close_fds=True)
+	subprocess.Popen(["c:/apache24/bin/ab.exe", "-n","10000","-c","64","http://192.168.0.10:3000/db/",">","n_simple.txt"], shell=True, stdin=None, stdout=None, stderr=None, close_fds=True)
 
 def n_fireRegexp():
 	subprocess.Popen(["c:/apache24/bin/ab.exe", "-n","10","-c","1","http://192.168.0.10:3000/regexp/",">","n_regexp.txt"], shell=True, stdin=None, stdout=None, stderr=None, close_fds=True)
@@ -10,7 +10,7 @@ def n_fireStatic():
 	subprocess.Popen(["c:/apache24/bin/ab.exe", "-n","10","-c","1","http://192.168.0.10:3000/reactor.jpg",">","n_static.txt"], shell=True, stdin=None, stdout=None, stderr=None, close_fds=True)
 
 def p_fireSimple():
-	subprocess.Popen(["c:/apache24/bin/ab.exe", "-n","10000","-c","64","http://192.168.0.10/simpleString.php",">","p_simple.txt"], shell=True, stdin=None, stdout=None, stderr=None, close_fds=True)
+	subprocess.Popen(["c:/apache24/bin/ab.exe", "-n","10000","-c","64","http://192.168.0.10/db.php",">","p_simple.txt"], shell=True, stdin=None, stdout=None, stderr=None, close_fds=True)
 
 def p_fireRegexp():
 	subprocess.Popen(["c:/apache24/bin/ab.exe", "-n","10","-c","1","http://192.168.0.10/regexp.php",">","p_regexp.txt"], shell=True, stdin=None, stdout=None, stderr=None, close_fds=True)
