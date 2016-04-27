7cde8e6df12f859c885a967820480e404453dc6c
Steven Kitzes <winds23@gmail.com>
2016-04-27 10:17:57 -0700

    modified multiChildPing with updated IP address, created new results from sleeper replacement test

--- tools/multiChildPing.py
+++ tools/multiChildPing.py
@@ -23,22 +23,22 @@ if len(sys.argv) > 2:
 	mod = "_" + sys.argv[2]
 
 def n_fireDatabase():
-	subprocess.Popen(["c:/apache24/bin/ab.exe", "-n","10000","-c","64","http://192.168.29.236:3000/db/",">","../results/n_database" + mod + ".txt"], shell=True, stdin=None, stdout=None, stderr=None, close_fds=True)
+	subprocess.Popen(["c:/apache24/bin/ab.exe", "-n","10000","-c","64","http://192.168.0.10:3000/db/",">","../results/n_database" + mod + ".txt"], shell=True, stdin=None, stdout=None, stderr=None, close_fds=True)
 
 def n_fireRegexp():
-	subprocess.Popen(["c:/apache24/bin/ab.exe", "-n","30","-c","1","http://192.168.29.236:3000/regexpChildPing/",">","../results/n_regexp" + mod + ".txt"], shell=True, stdin=None, stdout=None, stderr=None, close_fds=True)
+	subprocess.Popen(["c:/apache24/bin/ab.exe", "-n","30","-c","1","http://192.168.0.10:3000/regexpChildPing/",">","../results/n_regexp" + mod + ".txt"], shell=True, stdin=None, stdout=None, stderr=None, close_fds=True)
 
 def n_fireStatic():
-	subprocess.Popen(["c:/apache24/bin/ab.exe", "-n","30","-c","1","http://192.168.29.236:3000/reactor.jpg",">","../results/n_static" + mod + ".txt"], shell=True, stdin=None, stdout=None, stderr=None, close_fds=True)
+	subprocess.Popen(["c:/apache24/bin/ab.exe", "-n","30","-c","1","http://192.168.0.10:3000/reactor.jpg",">","../results/n_static" + mod + ".txt"], shell=True, stdin=None, stdout=None, stderr=None, close_fds=True)
 
 def p_fireDatabase():
-	subprocess.Popen(["c:/apache24/bin/ab.exe", "-n","10000","-c","64","http://192.168.29.236/db.php",">","../results/p_database" + mod + ".txt"], shell=True, stdin=None, stdout=None, stderr=None, close_fds=True)
+	subprocess.Popen(["c:/apache24/bin/ab.exe", "-n","10000","-c","64","http://192.168.0.10/db.php",">","../results/p_database" + mod + ".txt"], shell=True, stdin=None, stdout=None, stderr=None, close_fds=True)
 
 def p_fireRegexp():
-	subprocess.Popen(["c:/apache24/bin/ab.exe", "-n","30","-c","1","http://192.168.29.236/regexpChildPing.php",">","../results/p_regexp" + mod + ".txt"], shell=True, stdin=None, stdout=None, stderr=None, close_fds=True)
+	subprocess.Popen(["c:/apache24/bin/ab.exe", "-n","30","-c","1","http://192.168.0.10/regexpChildPing.php",">","../results/p_regexp" + mod + ".txt"], shell=True, stdin=None, stdout=None, stderr=None, close_fds=True)
 
 def p_fireStatic():
-	subprocess.Popen(["c:/apache24/bin/ab.exe", "-n","30","-c","1","http://192.168.29.236/reactor.jpg",">","../results/p_static" + mod + ".txt"], shell=True, stdin=None, stdout=None, stderr=None, close_fds=True)
+	subprocess.Popen(["c:/apache24/bin/ab.exe", "-n","30","-c","1","http://192.168.0.10/reactor.jpg",">","../results/p_static" + mod + ".txt"], shell=True, stdin=None, stdout=None, stderr=None, close_fds=True)
 
 if which == "node":
 	n_fireStatic()
